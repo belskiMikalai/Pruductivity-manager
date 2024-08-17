e2b5b39087d43cd5b0b2fca4f7f192041b2ca13b
BelskiMikolai <146476748+BelskiMikolai@users.noreply.github.com>
2024-08-17 15:29:00 +0300

    create and configure DB

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+class MyTask(db.Model):
         id = db.Column(db.Integer, primary_key=True)
-
+        content = db.Column(db.String(100), nullable=False)
+        def __repr__(self) -> str:
+                return f"Task {self.id}"
 
 @app.route('/')
 def index():
         return render_template("index.html")
 
 if __name__ == "__main__":
+        with app.app_context():
+                db.create_all()
         app.run(debug=True)
